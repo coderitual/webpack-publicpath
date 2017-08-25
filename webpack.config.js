13337382c76202daa10f46c17ae7e7f9b7194fc7
coderitual <skowronkow@hotmail.com>
2017-08-25 23:33:04 +0200

    feat: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}  
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  }
+};
